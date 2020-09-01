5494adfc0110c9cac8b2ee97ac71536c892e8fb2
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-09-01 16:42:51 +1000

    also copy topog_edits.txt with copy_to_input_rc.sh

--- copy_to_input_rc.sh
+++ copy_to_input_rc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 set -e
 set -x
-cp --preserve=timestamps --remove-destination topog.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg/topog.nc
-cp --preserve=timestamps --remove-destination ocean_mask.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg/ocean_mask.nc
-cp --preserve=timestamps --remove-destination ocean_vgrid.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg/ocean_vgrid.nc
-cp --preserve=timestamps --remove-destination kmt.nc /g/data/ik11/inputs/access-om2/input_rc/cice_1deg/kmt.nc
+cp --preserve=timestamps --remove-destination topog.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
+cp --preserve=timestamps --remove-destination ocean_mask.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
+cp --preserve=timestamps --remove-destination ocean_vgrid.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
+cp --preserve=timestamps --remove-destination kmt.nc /g/data/ik11/inputs/access-om2/input_rc/cice_1deg
+cp --preserve=timestamps --remove-destination topog_edits.txt /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
