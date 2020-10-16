f5347ee91ac75ccfcda185a5063cd5ce8250de2f
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-10-16 11:10:10 +1100

    improved copy_to_input_rc.sh

--- copy_to_input_rc.sh
+++ copy_to_input_rc.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env sh
+
+outpath=/g/data/ik11/inputs/access-om2/input_rc
+
+echo "WARNING: About to copy files to ${outpath}"
+read -p "Proceed? (y/n) " yesno
+case $yesno in
+   [Yy] ) ;;
+      * ) echo "Cancelled."; exit 0;;
+esac
+
 set -e
 set -x
-cp --preserve=timestamps --remove-destination topog.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
-cp --preserve=timestamps --remove-destination ocean_mask.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
-cp --preserve=timestamps --remove-destination ocean_vgrid.nc /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
-cp --preserve=timestamps --remove-destination kmt.nc /g/data/ik11/inputs/access-om2/input_rc/cice_1deg
-cp --preserve=timestamps --remove-destination topog_edits.txt /g/data/ik11/inputs/access-om2/input_rc/mom_1deg
+
+cp --preserve=timestamps --remove-destination topog.nc ${outpath}/mom_1deg
+cp --preserve=timestamps --remove-destination ocean_mask.nc ${outpath}/mom_1deg
+cp --preserve=timestamps --remove-destination ocean_vgrid.nc ${outpath}/mom_1deg
+cp --preserve=timestamps --remove-destination kmt.nc ${outpath}/cice_1deg
+
+set +e
+chgrp ik11 ${outpath}/mom_1deg/* ${outpath}/cice_1deg/*
+chmod g+r  ${outpath}/mom_1deg/* ${outpath}/cice_1deg/*
+
+echo "done"
